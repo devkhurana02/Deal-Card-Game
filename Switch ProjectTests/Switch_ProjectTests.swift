768e1611fab7b1a514b35b1cbb1e9a8e49904251
devkhurana02 <devkhurana432@gmail.com>
2025-04-27 00:14:31 +0530

    Initial Commit

---
+++ Switch ProjectTests/Switch_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Switch_ProjectTests.swift
+//  Switch ProjectTests
+//
+//  Created by Dev Khurana on 27/04/25.
+//
+
+import Testing
+@testable import Switch_Project
+
+struct Switch_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
